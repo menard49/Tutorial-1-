8cd0ce670d989d500be461c3f6913844ec60c9f6
menard49 <amenard@knights.ucf.edu>
2020-01-30 13:43:46 -0500

    challenge 1

--- UFO Game/Assets/PlayerController.cs
+++ UFO Game/Assets/PlayerController.cs
@@ -2,58 +2,82 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
     public Text countText;
     public Text winText;
+    public Text livesText;
 
     private Rigidbody2D rb2d;
     private int count;
+    private int lives;
 
-    void Start ()
+    void Start()
 
-	{
+    {
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
+        lives = 3;
         winText.text = "";
         SetCountText();
-	}
+        SetLivesText();
+
+    }
 
 
     void FixedUpdate()
-	{
-        float moveHorizontal = Input.GetAxis ("Horizontal");
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce (movement * speed);
-	}
+        rb2d.AddForce(movement * speed);
+    }
 
     void OnTriggerEnter2D(Collider2D other)
-	{
-        if (other.gameObject.CompareTag ("PickUp"))
-		{
-            other.gameObject.SetActive (false);
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
-		}
-	}
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+
+        if (count == 12)
+        {
+            transform.position = new Vector2(50.0f, 50.0f);
+        }
+    }
 
     void SetCountText()
-	{
+    {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
-		{
-            winText.text = "You win!";
-		}
-	}
+        if (count >= 20)
+        {
+            winText.text = "You Win! :By Austin Menard";
 
-    void update()
-	{
-        if (Input.GetKey("escape"))
-		{
-            Application.Quit();
-		}
-	}
+        }
+
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+            winText.text = "You lose! :By Austin Menard";
+
+        }
+    }
+
+
 }
  
